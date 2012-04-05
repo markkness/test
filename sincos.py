3d58d2ce05d28f04f7142c08070143e351d2699d
Mark Kness <mkness@enthought.com>
2012-04-05 14:35:02 -0500

    Added test for sin*sin + cos*cos = 1

--- sincos.py
+++ sincos.py
@@ -1,6 +1,8 @@
 #!/home/mkness/epd/bin/python
 '''
 sincos.py - Test program for git practice.
+
+Calculates sin(n*x), cos(n*x) for n=1..N using recurrence formula.
 '''
 
 import math
@@ -62,8 +64,10 @@ def sincos_print(x, n, sc):
             j, x, sj, j, x, cj))
 
 #
-# Tests - Calculate also with explicit trig formulas and compare to recurrence.
+# Tests
 #
+
+# Calculate also with explicit trig formulas and compare to recurrence.
 
 def sincos_all_raw(x, n):
     '''Calculate sin(m*x), cos(m*x) for m = 1..n, as a numpy array, with explicit trig calls.
@@ -102,6 +106,41 @@ def sincos_all_test(x, n, verbose=False, tol=1.0e-10):
     ok = (errmag <= tol)
     return ok
 
+# Check that sin*sin + cos*cos = 1
+
+def sincos_check_unit(sc, verbose=False, tol=1.0e-10):
+    '''Check that sin*sin + cos*cos = 1 for each row.'''
+    shape = sc.shape
+    if len (shape) != 2:
+        # expecting 2d array
+        return False
+    (nrows, ncols) = shape
+    if ncols != 2:
+        # expecting 2 columns, sin,cos
+        return False
+    errsum = 0.0
+    for irow in range(nrows):
+        si = sc[irow, 0]
+        ci = sc[irow, 1]
+        one = si*si + ci*ci
+        err = math.fabs (one - 1.0)
+        errsum += err
+        if err > tol:
+            # error too large
+            return False
+    if verbose:
+        print('errsum = %g' % (errsum))
+    # all checks passed
+    return True
+
+def sincos_check_unit_test(x, n, verbose=False, tol=1.0e-10):
+    '''Return whether the error in sincos_all() is <= tol.'''
+    sc = sincos_all(x, n)
+    rtn = sincos_check_unit(sc, verbose=verbose, tol=tol)
+    return rtn
+
+# Driver to run all tests for multiple cases
+
 def sincos_all_test_many(m, verbose=False, tol=1.0e-10):
     '''Run m tests and return the count of success, failure as a tuple.'''
     random.seed()
@@ -111,6 +150,7 @@ def sincos_all_test_many(m, verbose=False, tol=1.0e-10):
         # get random angle and max index
         x = random.uniform(-10.0, 10.0)
         n = random.randint(1, 100)
+        # test against explicit trig calls
         if verbose:
             print('Testing sincos(x=% -10g, n=%3d) ...' % (x, n)),
         ok = sincos_all_test(x, n, verbose, tol)
@@ -118,6 +158,15 @@ def sincos_all_test_many(m, verbose=False, tol=1.0e-10):
             good += 1
         else:
             fail += 1
+        # test for sin*sin + cos*cos = 1
+        if verbose:
+            print('Testing sin*sin + cos*cos = 1 (x=% -10g, n=%3d) ...' % (x, n)),
+        ok = sincos_check_unit_test(x, n, verbose, tol)
+        if ok:
+            good += 1
+        else:
+            fail += 1
+            
     if verbose:
         print('Passed %d tests, Failed %d tests.' % (good, fail))
     rtn = (good, fail)
@@ -127,23 +176,28 @@ def test():
     '''Test driver.'''
     x = 2.718281828
     #x = 0.01
-    (s1,c1) = sincos(x)
-    print('sin(%d * %g) = %g, cos(%g) = %g' % (
-        1, x, s1, x, c1))
-    (s2,c2) = sincos_n(s1, c1, s1, c1)
-    print('sin(%d * %g) = %g, cos(%g) = %g' % (
-        2, x, s2, x, c2))
-    (s3,c3) = sincos_n(s1, c1, s2, c2)
-    print('sin(%d * %g) = %g, cos(%g) = %g' % (
-        3, x, s3, x, c3))
-    scn = sincos_all(x, 8)
-    print 'scn=', scn
+    # calculate for n=1..3
+    sc3a = sincos_all(x, 3)
+    sc3b = sincos_all_raw(x, 3)
+    print('Calculations with recurrence formula...')
+    sincos_print(x, 3, sc3a)
+    print('Calculations with explicit trig...')
+    sincos_print(x, 3, sc3b)
+    # calculate for n=1..8
+    sc8 = sincos_all(x, 8)
+    print('Recurrence for n=1..8...')
+    sincos_print(x, 8, sc8)
+    # test case for n=1..8
+    print('Test case for n=1..8...')
     sincos_all_error(x, 8)
     sincos_all_errmag(x, 8)
     ok = sincos_all_test(x, 8, verbose=True)
-    print 'ok =', ok
-    sincos_all_test_many(10, verbose=True)
-    sincos_print(x, 8, scn)
+    print('ok = %s' % str(ok))
+    # s*s + c*c = 1 test
+    sincos_check_unit(sc8, verbose=True)
+    # multiple test cases
+    print('Multiple test cases...')
+    sincos_all_test_many(10, verbose=True, tol=1.0e-10)
     
 test()
 
