ab540252185b3306bf3645fe0edcbecf636e8563
Mark Kness <mkness@enthought.com>
2012-04-09 15:20:19 -0500

    Added test Qt program for PySide

---
+++ qt_test.py
@@ -0,0 +1,18 @@
+'''
+qt_test.py - Qt test program to check PySide installation.
+
+Taken from:
+http://en.wikipedia.org/wiki/PySide
+'''
+
+import sys
+from PySide import QtCore, QtGui
+
+app = QtGui.QApplication(sys.argv)
+win = QtGui.QWidget()
+
+win.resize(320, 240)
+win.setWindowTitle("Hello, World!")
+win.show()
+
+sys.exit(app.exec_())
